5ddda9cdb4db442f2ae5c024a42e0929561af265
Jon Škerlj <jon.skerlj17@imperial.ac.uk>
2019-03-10 13:37:07 +0000

    YEEET

---
+++ google-images-download-master/google_images_download/example1.py
@@ -0,0 +1,20 @@
+from google_images_download import google_images_download   #importing the library
+
+response = google_images_download.googleimagesdownload()   #class instantiation
+
+{
+    "Records": [
+        {
+            "keywords": "apple",
+            "limit": 5,
+            "color": "green",
+            "print_urls": true
+        },
+        {
+            "keywords": "universe",
+            "limit": 15,
+            "size": "large",
+            "print_urls": true
+        }
+    ]
+}
